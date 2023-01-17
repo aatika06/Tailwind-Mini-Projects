92010ba958def5e2685009a6d8812560599ec6c5
Aatika Almas <122785448+aatika06@users.noreply.github.com>
2023-01-17 14:38:03 +0500

    Clipboard

---
+++ Tailwind Website Projects/Clipboard/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        strongcyan: 'hsl(171, 66%, 44%)',
+        lightblue: 'hsl(233, 100%, 69%)',
+        darkgrayishblue: 'hsl(210, 10%, 33%)',
+        grayishblue: 'hsl(201, 11%, 66%)',
+      },
+      fontfamily: {
+        sans: ['Bai Jamjuree', 'sans-serif'],
+      },
+    },
+  },
+    plugins: [],
+}
